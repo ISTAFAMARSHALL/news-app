ec02a0cf078f06b2d3a4719cec1134ae3aa352cf
istafamarshall <istafa.a.marshall@gmail.com>
2024-07-08 20:47:27 -0400

    update news details Page with dummy data

--- app/news/[id]/page.js
+++ app/news/[id]/page.js
@@ -1,9 +1,24 @@
+import { DUMMY_NEWS } from "@/dummy-news"
+
 export default function NewsDetailsPage (props) {
+
+    const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.id === props.params.id )
+
     return (
-        <div>
+        <article>
+
+            <header>
+
+                <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+                
+                <h1>{newsItem.title}</h1>
+
+                <time dateTime={newsItem.date} >{newsItem.date}</time>
             
-            <h1> I am the Details Page of news {props.params.id}</h1>
-            
-        </div>
+            </header>
+
+            <p>{newsItem.content}</p>
+
+        </article>
     )
 }
