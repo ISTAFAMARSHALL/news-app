a5765358924d16eacfc0729b85123724aaf51c46
istafamarshall <istafa.a.marshall@gmail.com>
2024-07-08 20:30:17 -0400

    create dummy-news.js

--- app/news/page.js
+++ app/news/page.js
@@ -1,7 +1,32 @@
+import Link from "next/link"
+
 export default function NewsPage () {
     return (
-        <div>
+        <>
             <h1>Hello This is the News Page</h1>
-        </div>
+
+            <ul className='news-list'>
+
+                <li>
+
+                    <Link href={'/news/1'}>First Link</Link>
+
+                </li>
+
+                <li>
+
+                    <Link href={'/news/2'}>Second Link</Link>
+
+                </li>
+
+                <li>
+
+                    <Link href={'/news/3'}>Third Link</Link>
+
+                </li>
+
+            </ul>
+
+        </>
     )
 }
--- app/page.js
+++ app/page.js
@@ -6,28 +6,6 @@ export default function HomePage () {
 
       <h1>News App Home Page</h1>
 
-      <ul>
-
-        <li>
-
-          <Link href={'/news/1'}>First Link</Link>
-
-        </li>
-
-        <li>
-
-          <Link href={'/news/2'}>Second Link</Link>
-
-        </li>
-
-        <li>
-
-          <Link href={'/news/3'}>Third Link</Link>
-
-        </li>
-
-      </ul>
-
     </div>
 
   )
